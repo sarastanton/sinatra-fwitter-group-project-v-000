ef6184be404879682cca6b41308bcb11663ca1ad
Sara Stanton <sara.mpa13@gmail.com>
2018-07-03 06:34:59 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,31 @@
 class TweetsController < ApplicationController
 
+  get "/" do
+    erb :"/tweets/homepage"
+  end
+
+  get "/tweets" do
+    erb :"/tweets/index"
+  end
+
+  get "/tweets/new" do
+    erb :"/tweets/new"
+  end
+
+  get "/tweets/:id" do
+    erb :"/tweets/show"
+  end
+
+  get "/tweets/:id/edit" do
+    erb :"/tweets/edit"
+  end
+
+  patch "/tweets/:id" do
+
+  end
+
+  delete "/tweets/:id/delete" do
+    erb :"/tweets/delete"
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
 class UsersController < ApplicationController
 
+  get "/signup" do
+    erb :"/users/signup"
+  end
+
+  post "/signup" do
+    @user = User.new(username: params[:username], password: params[:password])
+    @user.save
+    session[:id] = @user.id
+
+    if user.save
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
+  end
+
+  post "/login" do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/success"
+    else
+      redirect "/failure"
+    end
+  end
+
+  get "/success" do
+    erb :success
+  end
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+  end
 
 end
