fda75b82d552ce2ff200edb651c3838e20190ab6
Sara Stanton <sara.mpa13@gmail.com>
2018-07-12 07:11:13 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   include Slugifiable::InstanceMethods
   extend Slugifiable::ClassMethods
-  
+
   has_many :tweets
   has_secure_password
   validates_presence_of :username, :email, :password
