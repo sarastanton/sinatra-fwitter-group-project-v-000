4d27ea9a3e7865997d7a0e6bb0e6aff4a509c504
Sara Stanton <sara.mpa13@gmail.com>
2018-07-12 07:11:50 +0000

    automatically backed up by learn

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -2,7 +2,7 @@ module Slugifiable
 
   module ClassMethods
     def find_by_slug(slug)
-      self.all.find{|name| username.slug == slug}
+      self.all.find{|username| username.slug == slug}
     end
   end
 
