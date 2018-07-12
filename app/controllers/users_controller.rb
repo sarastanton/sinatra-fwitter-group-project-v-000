5cd3f7c50af79bc621d3d0dee9755e8908792bae
Sara Stanton <sara.mpa13@gmail.com>
2018-07-12 06:54:00 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
   end
 
   get "/users/:id" do
-    @tweet = Tweet.find_by(user: params[:id])
+    @tweet = Tweet.find_by(user_id: params[:id])
     erb :"/users/show"
   end
 
